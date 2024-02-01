a8a08dd4ad83b453cb18969bb84fae9ea2f2c252
Florencia Baltar <gabriel.m.ledesma96@gmail.com>
2024-02-01 19:16:21 -0300

    url origin allowed

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config'
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://127.0.0.1:3000'
+    origin: process.env.URL_ALLOWED_1
   });
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
