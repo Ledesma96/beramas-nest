647fa4b3a6673a1548f2013f1bb9703ac6c8556d
gabriel <gabriel.m.ledesma96@gmail.com>
2024-02-06 21:52:35 -0300

    url

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import 'dotenv/config'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.URL_ALLOWED_1, 'http://127.0.0.1:3001']
+    origin: [process.env.URL_ALLOWED_1, process.env.URL_ALLOWED_2,'http://127.0.0.1:3001']
   });
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
