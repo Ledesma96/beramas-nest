08208dc172b5c3d8b64426e56b7cf70b09d1e31e
gabriel <gabriel.m.ledesma96@gmail.com>
2024-02-07 13:38:09 -0300

    agrego imagenes

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -67,7 +67,6 @@ export class ProductsService {
     async updateProduct(data: UpdateProductsDto, id: string, files: Express.Multer.File[]): Promise<{success: boolean, message: string}>{
         try {
             data.thumbnail = files.map(file => `/upload/products/${file.filename}`)
-            console.log(data.thumbnail)
             const result = await this.productModel.findByIdAndUpdate(id, data);
             if(result){
                 return { success: true, message:'Product updated successfully'}
